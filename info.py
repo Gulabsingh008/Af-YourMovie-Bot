547a4f9261bceb96aad1bb6aa27f42c8cf34a959
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-07-15 21:38:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'knownlinks.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '4dbeb3373153a8396dd23d2e9da53c8f24f4b449')
